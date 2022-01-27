a7d6938515c4146a096eab1678e2f2803e164247
aelmouny11 <azddine.elmoumny@edu.uca.ma>
2022-01-27 19:15:18 +0100

    Ajouter le construicteur et le destructreur

--- class.cpp
+++ class.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 using namespace std;
-class Personne{
-    private:
-    string CIN,Nom,Prenom;
-    public:
-    void initialize(string ci,string No,string Pr){
+class Personne
+{
+private:
+    string CIN, Nom, Prenom;
+
+public:
+    Personne(string ci, string No,string Pr){
         CIN = ci;
-        Nom=No;
-        Prenom=Pr;
-
+        Nom = No;
+        Prenom = Pr;
     }
-    void affiche(){
-        cout<<"["<<CIN<<","<<Nom<<","<<Prenom<<"]"<<endl;
+    void affiche()
+    {
+        cout << "[" << CIN << "," << Nom << "," << Prenom << "]" << endl;
     }
-
+    ~Personne(){
+        cout << "[" << CIN << "," << Nom << "," << Prenom << "]" << " a été supprimé";
+    }
 };
-int main(){
+int main()
+{
     // int B;
-    Personne A;
-    A.affiche();
+    Personne *A=new Personne("EE4","Ilham","Elaraji");
+    A->affiche();// - >
+    delete A;
 }
+
