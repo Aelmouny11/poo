db9278d68e1722f1148767e7fc715603e6dff35b
aelmouny11 <azddine.elmoumny@edu.uca.ma>
2022-01-27 15:04:52 +0100

    Les valeurs par default

--- surcharge.cpp
+++ surcharge.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int add(int a,int b){
     return a + b;
 }
-float add(double a,double b){
-    return a + b;
+float add(double a,double b,int c=5){
+    return a + b + c;
 }
 
 
 int main(){
     printf("int : %d\n",add(1,1));
-    printf("float : %f\n",add(5.1,2.1));
+    printf("float : %f\n",add(5.1,2.1,12));
     return 0;
 }
